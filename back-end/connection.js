7e312cdd514faec4258fdf1a3da3b5498b7d2a68
sharonshiju5 <sharonshiju261@gmail.com>
2025-07-25 13:39:23 +0530

    placeholder

--- back-end/connection.js
+++ back-end/connection.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-export default async function connetion(){
+export default async function connection(){
     const db=await mongoose.connect(process.env.DB_URL+process.env.DB_NAME);
     console.log("database connected");
     return db
